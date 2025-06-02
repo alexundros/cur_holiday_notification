83824afc5024ed3f8ef3c0c1aaf7727a07174a27
alexundros <alex-vlz@mail.ru>
2025-06-02 21:43:53 +0300

    updated start-test-builds

--- start-test-builds.bat
+++ start-test-builds.bat
@@ -1,20 +1,28 @@
 @echo off
 @chcp 1251 > nul
 cd /d "%~dp0" || exit 1
+@setlocal enabledelayedexpansion
 
+set c=0
 for %%a in (test_xmlfiles\*.xml) do (
-  echo # TEST XML: %%a
+  set /a c=!c!+1
   echo.
-  echo # PY_PROJECT main.py:
+  echo # TEST FILE !c!: %%a
+  echo.
+  echo # PY_PROJECT main.py - TEST FILE !c!:
+  echo.
   python py_project\src\main.py "%%a" true
   echo.
-  echo # PY_PROJECT:
+  echo # PY_PROJECT - TEST FILE !c!:
+  echo.
   py_project\build\win-amd64\py_project.exe "%%a" true
   echo.
-  echo # GO_PROJECT:
+  echo # GO_PROJECT - TEST FILE !c!:
+  echo.
   go_project\build\go_project.exe "%%a" true
   echo.
-  echo # RUST_PROJECT:
+  echo # RUST_PROJECT - TEST FILE !c!:
+  echo.
   rust_project\target\release\rust_project.exe "%%a" true
 )
 pause
