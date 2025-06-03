f48ef0c166d6fab912a4814f320f7946a18a57a1
alexundros <alex-vlz@mail.ru>
2025-06-03 09:25:14 +0300

    updated start-build-all

--- start-build-all.bat
+++ start-build-all.bat
@@ -2,12 +2,18 @@
 @chcp 1251 > nul
 cd /d "%~dp0" || exit 1
 
+echo.
 echo # BUILD PY_PROJECT:
+echo.
 cd "%~dp0py_project"
 python cx_setup___main.py build --compiler=mingw32
-echo. & echo # BUILD GO_PROJECT:
+echo. 
+echo # BUILD GO_PROJECT:
+echo.
 cd "%~dp0go_project"
 go build -ldflags="-s -w" -trimpath -buildvcs=false -o build/go_project.exe
-echo. & echo # BUILD RUST_PROJECT:
+echo.
+echo # BUILD RUST_PROJECT:
+echo.
 cd "%~dp0rust_project"
 cargo build --release
