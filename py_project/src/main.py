1f7b91d9ae7c375276a3d90d71d76cec4b0478c0
alexundros <alex-vlz@mail.ru>
2025-06-18 12:47:39 +0300

    fixed py project

--- py_project/src/main.py
+++ py_project/src/main.py
@@ -3,31 +3,23 @@ import os
 import sys
 import time
 import traceback
+
 # noinspection PyPep8Naming
 import xml.etree.ElementTree as ET
 from configparser import ConfigParser
 from pathlib import Path
 
-if getattr(sys, 'frozen', False):
-    os.add_dll_directory(f'{Path(sys.executable).parent}/dll')
+if getattr(sys, "frozen", False):
+    os.add_dll_directory(f"{Path(sys.executable).parent}/dll")
 
 
 # noinspection PyMissingOrEmptyDocstring
-def main_process(appdir, workdir):
-    xml = get_xml_file()
-    start = time.time()
-    config = get_config(appdir, workdir)
-
-    print(f'# Обработка: {xml}')
-
-    result = process_xml(config, xml)
-    write_results(result, workdir)
-
-    print('# Обработка завершена: {:.6f} сек.'.format(time.time() - start))
+def print_elapsed_time(text, start, end):
+    print("# {}: {:.9f} сек.".format(text, end - start))
 
 
 # noinspection PyMissingOrEmptyDocstring
-def config_items(config: ConfigParser, section='default'):
+def config_items(config: ConfigParser, section="default"):
     try:
         return config.items(section)
     except configparser.Error:
@@ -35,62 +27,83 @@ def config_items(config: ConfigParser, section='default'):
 
 
 # noinspection PyMissingOrEmptyDocstring
+def main_process(appdir, workdir):
+    xml = get_xml_file()
+    start = time.time()
+
+    config = get_config(appdir, workdir)
+    end_get_config = time.time()
+
+    result = process_xml(config, xml)
+    end_process_xml = time.time()
+
+    write_results(result, workdir)
+    end = time.time()
+
+    print_elapsed_time("* get config", start, end_get_config)
+    print_elapsed_time("* process xml", end_get_config, end_process_xml)
+    print_elapsed_time("* process results", end_process_xml, end)
+    print_elapsed_time("Обработка завершена", start, end)
+
+
+# noinspection PyMissingOrEmptyDocstring
 def get_xml_file():
     if len(sys.argv) > 1:
         file = sys.argv[1]
     else:
-        if (file := input('# XML файл: ')) == '':
+        if (file := input("# XML файл: ")) == "":
             return get_xml_file()
     if os.path.exists(file):
         return os.path.abspath(file)
-    raise FileNotFoundError(f'# {file} не найден')
+    raise FileNotFoundError(f"# {file} не найден")
 
 
 # noinspection PyMissingOrEmptyDocstring
 def get_config(appdir: str, workdir: str) -> ConfigParser:
-    cfg_name = 'cur_holiday_notification.cfg'
+    cfg_name = "cur_holiday_notification.cfg"
     cfg_file = os.path.join(workdir, cfg_name)
     if not os.path.exists(cfg_file):
         cfg_file = os.path.join(appdir, cfg_file)
         if not os.path.exists(cfg_file):
-            raise FileNotFoundError(f'# Конфиг {cfg_name} не найден')
+            raise FileNotFoundError(f"# Конфиг {cfg_name} не найден")
     config = ConfigParser()
     config.optionxform = str
     cfg_file = os.path.abspath(cfg_file)
-    with open(cfg_file, encoding='cp1251') as fp:
+    with open(cfg_file, encoding="cp1251") as fp:
         config.read_file(fp)
-    print(f'# Используем конфиг: {cfg_file}')
+    print(f"# Используем конфиг: {cfg_file}")
     return config
 
 
 # noinspection PyMissingOrEmptyDocstring
 def process_xml(config, xml):
-    fhd = config.getboolean('features', 'HDay')
-    items = config_items(config, 'items')
+    print(f"# Обработка: {xml}")
+    fhd = config.getboolean("features", "HDay")
+    items = config_items(config, "items")
     result = []
-    for item in ET.parse(xml).getroot().findall('./CUX50'):
+    for item in ET.parse(xml).getroot().findall("./CUX50"):
         if fhd:
-            if item.find(f"./GROUP[@TradeGroup='H']") is not None:
-                result.append(('HDay', item.get('ReportDate')))
+            if item.find("./GROUP[@TradeGroup='H']") is not None:
+                result.append(("HDay", item.get("ReportDate")))
                 continue
         for k, v in items:
             if item.find(f".//SECURITY[@SecShortName='{k}']") is not None:
-                result.append((v, item.get('ReportDate')))
+                result.append((v, item.get("ReportDate")))
     return result
 
 
 # noinspection PyMissingOrEmptyDocstring
 def write_results(result, workdir):
-    out = os.path.join(workdir, 'py_project.out')
-    with open(out, 'w', encoding='cp1251') as of:
+    out = os.path.join(workdir, "py_project.out")
+    with open(out, "w", encoding="cp1251") as of:
         of.truncate()
         if result:
-            print(f'# Результат:')
+            print("# Результат:")
             for ri in result:
-                of.write(f'{ri[0]} = {ri[1]}\n')
-                print(f'{ri[0]} = {ri[1]}')
+                of.write(f"{ri[0]} = {ri[1]}\n")
+                print(f"{ri[0]} = {ri[1]}")
         else:
-            print('# Нет результата')
+            print("# Нет результата")
         print(f"# Результат сохранен в файл: {out}")
 
 
@@ -99,13 +112,13 @@ def main():
     appdir = os.path.dirname(sys.argv[0])
     appdir = os.path.abspath(appdir)
     workdir = os.path.abspath(os.curdir)
-    print(f'# Директория приложения: {appdir}')
-    print(f'# Рабочая директория: {workdir}')
+    print(f"# Директория приложения: {appdir}")
+    print(f"# Рабочая директория: {workdir}")
 
     is_error = True
     f_mode = False
     if len(sys.argv) > 2:
-        f_mode = sys.argv[2] == 'true'
+        f_mode = sys.argv[2] == "true"
 
     try:
         main_process(appdir, workdir)
@@ -113,19 +126,19 @@ def main():
     except FileNotFoundError as ex:
         print(ex)
     except KeyboardInterrupt:
-        print('\n# Работа программы прервана досрочно')
+        print("\n# Работа программы прервана досрочно")
     except Exception as ex:
-        print('# Не могу создать/записать файл результата')
-        err = traceback.format_exc().replace('\n', '\n# ')
-        print(f'# Программа прервана по ошибке: {ex}%n {err}')
+        print("# Не могу создать/записать файл результата")
+        err = traceback.format_exc().replace("\n", "\n# ")
+        print(f"# Программа прервана по ошибке: {ex}%n {err}")
 
     try:
         if not f_mode:
-            input('# Нажмите <Enter> для выхода')
+            input("# Нажмите <Enter> для выхода")
     except (KeyboardInterrupt, UnicodeDecodeError, EOFError):
         pass
     sys.exit(1 if is_error else 0)
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     main()
