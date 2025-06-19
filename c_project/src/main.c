9ea6d95f8b2b87e63c771802761985a26fc20a33
alexundros <alex-vlz@mail.ru>
2025-06-19 10:53:14 +0300

    updated wip c_project with fixed paths

--- c_project/src/main.c
+++ c_project/src/main.c
@@ -8,10 +8,57 @@
 
 #ifdef _WIN32
 #include <windows.h>
+#define PATH_SEP '\\'
+#else
+#define PATH_SEP '/'
+#define MAX_PATH 1024
 #endif
 
 #include "utils/ini_utils.h"
 #include "utils/xml_utils.h"
+
+#ifdef _WIN32
+
+char* get_appdir() {
+    static char path[MAX_PATH];
+    DWORD len = GetModuleFileNameA(NULL, path, sizeof(path));
+    if (len == 0 || len >= sizeof(path)) return NULL;
+    for (int i = len - 1; i >= 0; --i) {
+        if (path[i] == '\\') {
+            path[i] = '\0';
+            break;
+        }
+    }
+    return path;
+}
+
+int get_full_path(const char* relative, char* full, size_t size) {
+    if (GetFullPathName(relative, size, full, NULL) == 0) {
+        return -1;
+    }
+    return 0;
+}
+
+#else
+
+char* get_appdir() {
+    static char path[MAX_PATH];
+    ssize_t len = readlink("/proc/self/exe", path, sizeof(path) - 1);
+    if (len == -1) return NULL;
+    path[len] = '\0';
+    char* slash = strrchr(path, '/');
+    if (slash) *slash = '\0';
+    return path;
+}
+
+int get_full_path(const char* relative, char* full, size_t size) {
+    if (realpath(relative, full) == NULL) {
+        return -1;
+    }
+    return 0;
+}
+
+#endif
 
 static inline struct timespec get_timespec() {
     struct timespec ts;
@@ -31,14 +78,16 @@ static int file_exists(const char* path) {
 }
 
 static char* get_xml_file(int argc, char* argv[]) {
+    char path[MAX_PATH];
     if (argc > 1) {
         if (!file_exists(argv[1])) {
             fprintf(stderr, "# %s не найден\n", argv[1]);
             return NULL;
         }
-        return strdup(argv[1]);
+        get_full_path(argv[1], path, sizeof(path));
+        return strdup(path);
     } else {
-        char buf[512];
+        char buf[MAX_PATH];
         printf("# XML файл: ");
         if (fgets(buf, sizeof(buf), stdin) == NULL) {
             return NULL;
@@ -51,55 +100,24 @@ static char* get_xml_file(int argc, char* argv[]) {
             fprintf(stderr, "# %s не найден\n", buf);
             return get_xml_file(argc, argv);
         }
-        return strdup(buf);
+        get_full_path(buf, path, sizeof(path));
+        return strdup(path);
     }
 }
 
-static char* get_config_path(const char* appdir) {
+static char* get_config_path(const char* appdir, const char* workdir) {
     const char* name = "cur_holiday_notification.cfg";
-    char buf[1024];
-    snprintf(buf, 1024, "%s", name);
+    char buf[MAX_PATH];
+    snprintf(buf, MAX_PATH, "%s%c%s", workdir, PATH_SEP, name);
     if (file_exists(buf)) {
         return strdup(buf);
     }
-    snprintf(buf, 1024, "%s/%s", appdir, name);
+    snprintf(buf, MAX_PATH, "%s%c%s", appdir, PATH_SEP, name);
     if (file_exists(buf)) {
         return strdup(buf);
     }
     return NULL;
 }
-
-#ifdef _WIN32
-
-char* get_appdir() {
-    static char path[MAX_PATH];
-    DWORD len = GetModuleFileNameA(NULL, path, sizeof(path));
-    if (len == 0 || len >= sizeof(path)) return NULL;
-    for (int i = len - 1; i >= 0; --i) {
-        if (path[i] == '\\') {
-            path[i] = '\0';
-            break;
-        }
-    }
-    return path;
-}
-
-#else
-
-#include <libgen.h>
-#include <unistd.h>
-
-char* get_appdir() {
-    static char path[1024];
-    ssize_t len = readlink("/proc/self/exe", path, sizeof(path) - 1);
-    if (len == -1) return NULL;
-    path[len] = '\0';
-    char* slash = strrchr(path, '/');
-    if (slash) *slash = '\0';
-    return path;
-}
-
-#endif
 
 int main(int argc, char* argv[]) {
 #ifdef _WIN32
@@ -131,7 +149,7 @@ int main(int argc, char* argv[]) {
 
     struct timespec ts_config_s = get_timespec();
 
-    char* config_path = get_config_path(appdir);
+    char* config_path = get_config_path(appdir, workdir);
     if (!config_path) {
         fprintf(stderr, "# Конфиг не найден\n");
         return 2;
@@ -168,8 +186,8 @@ int main(int argc, char* argv[]) {
 
     // Вывод результата
 
-    char out_path[1024];
-    snprintf(out_path, 1024, "%s/c_project.out", workdir);
+    char out_path[MAX_PATH];
+    snprintf(out_path, MAX_PATH, "%s%cc_project.out", workdir, PATH_SEP);
     FILE* fo = fopen(out_path, "w");
     if (!fo) {
         fprintf(stderr, "# Не могу создать/записать файл результата: %s\n", out_path);
@@ -178,7 +196,9 @@ int main(int argc, char* argv[]) {
     if (results.count > 0) {
         fputs("# Результат:\n", stdout);
         for (int i = 0; i < results.count; i++) {
-            snprintf(text_buf, 1024, "%s = %s\n", results.items[i].code, results.items[i].date);
+            char* code = results.items[i].code;
+            char* date = results.items[i].date;
+            snprintf(text_buf, 1024, "%s = %s\n", code, date);
             fputs(text_buf, fo);
             fputs(text_buf, stdout);
         }
