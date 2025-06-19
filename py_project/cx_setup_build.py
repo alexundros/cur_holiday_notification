dd895b45f025b6f7123fc08d05a6de4223d47521
alexundros <alex-vlz@mail.ru>
2025-06-19 18:28:22 +0300

    updated py project build

--- py_project/cx_setup_build.py
+++ py_project/cx_setup_build.py
@@ -1,4 +1,5 @@
 import os
+import sys
 import shutil
 import sysconfig
 from pathlib import Path
@@ -12,43 +13,42 @@ class build_exe(_build_exe):
     def run(self):
         super().run()
         build_dir = Path(self.build_exe)
-        dll_dir = build_dir / 'dll'
+        dll_dir = build_dir / "dll"
         dll_dir.mkdir(exist_ok=True)
-        for pattern in ('*.dll', '*.pyd'):
+        for pattern in ("*.dll", "*.pyd"):
             for fi in build_dir.glob(pattern):
-                if fi.name.startswith('python'):
+                if fi.name.startswith("python"):
                     continue
                 shutil.move(str(fi), dll_dir / fi.name)
 
 
 platform = sysconfig.get_platform()
-build_out = os.path.join('build', platform)
+build_out = os.path.join("build", platform)
 
-with Path('VERSION').open() as file:
+with Path("VERSION").open() as file:
     version = file.read().strip()
 
-appname = 'py_project'
+appname = "py_project"
 cx_setup(
     name=appname,
     version=version,
-    description='Календарь торгов по валютам',
-    executables=[Executable(
-        'src/main.py', icon='src/assets/python', target_name=appname
-    )],
+    description="Календарь торгов по валютам",
+    executables=[Executable("src/main.py", icon="assets/python", target_name=appname)],
     cmdclass={"build_exe": build_exe},
     options={
-        'build_exe': {
-            'include_msvcr': True,
-            'excludes': [],
-            'includes': [],
-            'zip_include_packages': ['*'],
-            'zip_exclude_packages': [],
-            'build_exe': build_out,
-            'include_files': []
+        "build_exe": {
+            "include_msvcr": True,
+            "excludes": [],
+            "includes": [],
+            "zip_include_packages": ["*"],
+            "zip_exclude_packages": [],
+            "build_exe": build_out,
+            "include_files": [],
         }
-    }
+    },
 )
 
-zip_out = os.path.join('build', f'{appname}-{platform}-{version}')
-print(f'\nCreate application zip archive: {zip_out}.zip')
-shutil.make_archive(zip_out, 'zip', build_out)
+pv = f"{sys.version_info.major}.{sys.version_info.minor}"
+zip_out = os.path.join("build", f"{appname}-py{pv}-{platform}-{version}")
+print(f"\nCreate application zip archive: {zip_out}.zip")
+shutil.make_archive(zip_out, "zip", build_out)
